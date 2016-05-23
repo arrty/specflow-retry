1bdfbac644226833f5710a148f33e2b2451a09ef
DamirAinullin <damirainullin@outlook.com>
2016-05-23 07:52:46 +0000

    Add tests for Nunit

---
+++ src/Specflow.Retry.NunitSample/CriticalException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Specflow.Retry.NunitSample
+{
+    [Serializable]
+    public class CriticalException : Exception
+    {
+        public CriticalException()
+        {
+        }
+
+        public CriticalException(string message) : base(message)
+        {
+        }
+
+        public CriticalException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected CriticalException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
