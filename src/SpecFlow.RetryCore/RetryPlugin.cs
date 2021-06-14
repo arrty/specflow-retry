afc446e17c750eb3bf5b7fc4707b5379c290ae19
JTejero <jtejero@calrom.com>
2021-06-14 20:44:42 +0100

    dotnetcore migration

--- src/SpecFlow.RetryCore/RetryPlugin.cs
+++ src/SpecFlow.RetryCore/RetryPlugin.cs
@@ -1,14 +1,16 @@
-﻿using SpecFlow.Retry;
+﻿using SpecFlow.RetryCore;
 using TechTalk.SpecFlow.Generator.Plugins;
 using TechTalk.SpecFlow.Generator.UnitTestConverter;
 using TechTalk.SpecFlow.Infrastructure;
+using TechTalk.SpecFlow.UnitTestProvider;
 
-[assembly:GeneratorPlugin(typeof(GeneratorPlugin))]
-namespace SpecFlow.Retry
+[assembly: GeneratorPlugin(typeof(GeneratorPlugin))]
+namespace SpecFlow.RetryCore
 {
     public class GeneratorPlugin : IGeneratorPlugin
     {
-        public void Initialize(GeneratorPluginEvents generatorPluginEvents, GeneratorPluginParameters generatorPluginParameters)
+        public void Initialize(GeneratorPluginEvents generatorPluginEvents, GeneratorPluginParameters generatorPluginParameters,
+            UnitTestProviderConfiguration unitTestProviderConfiguration)
         {
             generatorPluginEvents.RegisterDependencies += (sender, args) =>
                 {
